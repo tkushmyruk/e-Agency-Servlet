23fd3f6ced7c823e6bc64716bce685335b78a494
taras kushmyruk <taras.kushmyruk@gmail.com>
2021-06-07 13:52:09 +0300

    add mock sql data for pagination

--- src/main/resources/sql/insert_data.sql
+++ src/main/resources/sql/insert_data.sql
@@ -1,10 +1,26 @@
 INSERT INTO usr (username, password,  email, is_active)
 VALUES('admin', 'admin', 'admin@gmail.com', true),
-('manager', 'manager', 'manager@gmail.com', true);
+('manager', 'manager', 'manager@gmail.com', true),
+('pageableUser1', '123', 'pageable@gmail.com', true),
+('pageableUser2', '123', 'pageable@gmail.com', true),
+('pageableUser3', '123', 'pageable@gmail.com', true),
+('pageableUser4', '123', 'pageable@gmail.com', true),
+('pageableUser5', '123', 'pageable@gmail.com', true),
+('pageableUser6', '123', 'pageable@gmail.com', true),
+('pageableUser7', '123', 'pageable@gmail.com', true),
+('pageableUser8', '123', 'pageable@gmail.com', true);
 
 INSERT INTO user_role(username, role)
 VALUES ('admin', 'ADMIN'),
-('manager', 'MANAGER');
+('manager', 'MANAGER'),
+('pageableUser1', 'USER'),
+('pageableUser2', 'USER'),
+('pageableUser3', 'USER'),
+('pageableUser4', 'USER'),
+('pageableUser5', 'USER'),
+('pageableUser6', 'USER'),
+('pageableUser7', 'USER'),
+('pageableUser8', 'USER');
 
 INSERT INTO tour(tour_name, price, count_of_people, start_date, end_date, departing_from,
  country, locality, hotel_name)
